18ea53cf10ad37affe5d981a36a920f7ab4f4172
Elanny Torres <torres.elanny@gmail.com>
2022-04-28 23:00:49 -0500

    search function

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Country } from '../../interfaces/pais.interface';
+
+import { PaisService } from '../../services/pais.service';
+
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `
   ]
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  activarRegion( region: string) {
+
+    if(region === this.regionActiva) { return; }
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion(region)
+      .subscribe( {
+        next: (paises) => {
+        console.log(paises);
+        this.paises = paises;
+        },
+        error: (err) => {
+        this.paises = [];
+        }
+      } );
   }
 
 }
